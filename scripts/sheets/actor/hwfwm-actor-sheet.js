f63649980d29de080a9f9cdd0317e36a64acc839
Shaladar79 <scott_d_anderson79@yahoo.com>
2026-07-15 09:10:38 -0500

    Refactor HWFWMActorSheet for improved structure

--- scripts/sheets/actor/hwfwm-actor-sheet.js
+++ scripts/sheets/actor/hwfwm-actor-sheet.js
@@ -1,91 +1,75 @@
 /**
- * Base Actor sheet for the HWFWM system.
+ * Base Actor sheet for HWFWM.
  *
- * Character, NPC, and monster sheets will initially share this class.
- * Specialized sheet classes can extend it later without duplicating
- * the common Actor-sheet behavior.
+ * Characters, NPCs, and monsters initially share this sheet class.
+ * Specialized sheets can extend this class later.
  */
-export class HWFWMActorSheet extends ActorSheet {
+
+const { ActorSheetV2 } = foundry.applications.sheets;
+const { HandlebarsApplicationMixin } = foundry.applications.api;
+
+export class HWFWMActorSheet extends HandlebarsApplicationMixin(ActorSheetV2) {
   /**
-   * Configure the sheet's default appearance and behavior.
-   *
-   * @returns {ActorSheetOptions}
+   * Configure the ApplicationV2 window and form behavior.
    */
-  static get defaultOptions() {
-    return foundry.utils.mergeObject(super.defaultOptions, {
-      classes: ["hwfwm", "sheet", "actor"],
+  static DEFAULT_OPTIONS = {
+    classes: ["hwfwm", "actor-sheet"],
+    position: {
       width: 720,
-      height: 680,
-      resizable: true,
-      tabs: [
-        {
-          navSelector: ".sheet-tabs",
-          contentSelector: ".sheet-body",
-          initial: "description"
-        }
-      ],
-      submitOnChange: true,
-      closeOnSubmit: false
+      height: 680
+    },
+    form: {
+      closeOnSubmit: false,
+      submitOnChange: true
+    },
+    window: {
+      resizable: true
+    }
+  };
+
+  /**
+   * Handlebars templates rendered by this application.
+   *
+   * Additional partial templates can be added as separate parts later.
+   */
+  static PARTS = {
+    form: {
+      template: "systems/hwfwm/templates/actor/actor-sheet.hbs"
+    }
+  };
+
+  /**
+   * Prepare data supplied to the Handlebars template.
+   *
+   * @param {object} options Rendering options.
+   * @returns {Promise<object>} Template context.
+   */
+  async _prepareContext(options) {
+    const context = await super._prepareContext(options);
+
+    const actor = this.actor;
+
+    return foundry.utils.mergeObject(context, {
+      actor,
+      system: actor.system,
+      actorType: actor.type,
+      editable: this.isEditable,
+
+      abilities: actor.items.filter(
+        (item) => item.type === "ability"
+      ),
+
+      essences: actor.items.filter(
+        (item) => item.type === "essence"
+      ),
+
+      equipment: actor.items.filter(
+        (item) => item.type === "equipment"
+      ),
+
+      consumables: actor.items.filter(
+        (item) => item.type === "consumable"
+      )
     });
-  }
-
-  /**
-   * Select the Handlebars template used by this Actor sheet.
-   *
-   * All Actor types use the same template initially. We can route
-   * character, NPC, and monster Actors to separate templates later.
-   *
-   * @returns {string}
-   */
-  get template() {
-    return "systems/hwfwm/templates/actor/actor-sheet.hbs";
-  }
-
-  /**
-   * Prepare data for the Handlebars sheet template.
-   *
-   * @param {ActorSheetOptions} options
-   * @returns {Promise<object>}
-   */
-  async getData(options = {}) {
-    const context = await super.getData(options);
-
-    context.actor = this.actor;
-    context.system = this.actor.system;
-    context.items = this.actor.items;
-    context.actorType = this.actor.type;
-    context.isEditable = this.isEditable;
-
-    context.abilities = this.actor.items.filter(
-      (item) => item.type === "ability"
-    );
-
-    context.essences = this.actor.items.filter(
-      (item) => item.type === "essence"
-    );
-
-    context.equipment = this.actor.items.filter(
-      (item) => item.type === "equipment"
-    );
-
-    context.consumables = this.actor.items.filter(
-      (item) => item.type === "consumable"
-    );
-
-    return context;
-  }
-
-  /**
-   * Attach interactive sheet listeners.
-   *
-   * Listener modules will be added here as interactive features
-   * such as ability rolls and item controls are implemented.
-   *
-   * @param {JQuery} html
-   */
-  activateListeners(html) {
-    super.activateListeners(html);
-
-    if (!this.isEditable) return;
   }
 }
