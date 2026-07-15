f9748725e19fbae3d1059c70810c450b7e4d8655
Shaladar79 <scott_d_anderson79@yahoo.com>
2026-07-15 10:16:53 -0500

    Add attribute configuration wrapper module

---
+++ scripts/config/attributes/index.js
@@ -0,0 +1,29 @@
+/**
+ * Attribute configuration wrapper.
+ *
+ * Other system files should import attribute definitions and calculation
+ * helpers through this wrapper instead of importing individual modules.
+ */
+
+export {
+  HWFWM_ATTRIBUTES,
+  HWFWM_ATTRIBUTE_ORDER,
+  HWFWM_ATTRIBUTE_LEVEL_RANGE
+} from "./attribute-definitions.js";
+
+export {
+  HWFWM_ATTRIBUTE_BASE_SCORES,
+  getAttributeBaseScore
+} from "./attribute-base-scores.js";
+
+export {
+  HWFWM_ATTRIBUTE_LEVEL_MULTIPLIER,
+  isValidAttributeLevel,
+  normalizeAttributeLevel,
+  getAttributeLevelModifier
+} from "./attribute-level-modifier.js";
+
+export {
+  normalizeAttributeModifier,
+  calculateAttribute
+} from "./attribute-calculator.js";
