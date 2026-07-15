c2a862f460da3b2d8484202bc39ecd57013b49ce
Shaladar79 <scott_d_anderson79@yahoo.com>
2026-07-15 10:24:45 -0500

    Add actor configuration wrapper module

---
+++ scripts/config/actors/index.js
@@ -0,0 +1,10 @@
+/**
+ * Actor configuration wrapper.
+ *
+ * Other system modules should import shared Actor defaults and helpers through
+ * this wrapper rather than importing individual Actor configuration files.
+ */
+
+export {
+  createDefaultActorData
+} from "./actor-defaults.js";
